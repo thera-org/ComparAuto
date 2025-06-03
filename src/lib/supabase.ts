3100defbd58af9be1d3dbf6efe89529d70651779
Marcos Vinicius <marcosviniciusmoraisrios117@gmail.com>
2025-06-02 22:47:59 -0300

    Criação da api da central de ajuda
    Api para o envio e armazenamento das mensagens

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 // src/lib/supabase.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Variáveis de ambiente do Supabase não configuradas!');
