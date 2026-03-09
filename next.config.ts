11d9b14aaff3ee125a2a169cb4ad27dc70f0cfad
Patrick <pm221100@gmail.com>
2026-03-09 13:32:44 -0300

    Corrigindo política para o "Minha Localização"

--- next.config.ts
+++ next.config.ts
@@ -95,7 +95,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Permissions-Policy',
-            value: 'camera=(), microphone=(), geolocation=()',
+            value: 'camera=(), microphone=(), geolocation=(self)',
           },
         ],
       },
